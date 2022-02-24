3c34fa4ccde53b0a01e6dfca7bbce1ac7c3571fc
sedat <sedatcyalcin@gmail.com>
2022-02-24 12:47:59 +0300

    lint

--- internal/utils/response.go
+++ internal/utils/response.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -14,12 +15,18 @@ func Error(w http.ResponseWriter, error Response, code int) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(code)
 	error.Status = "ERR"
-	json.NewEncoder(w).Encode(error)
+	err := json.NewEncoder(w).Encode(error)
+	if err != nil {
+		log.Printf("Error encoding json %s", err)
+	}
 }
 
 func Success(w http.ResponseWriter, success Response, code int) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(code)
 	success.Status = "OK"
-	json.NewEncoder(w).Encode(success)
+	err := json.NewEncoder(w).Encode(success)
+	if err != nil {
+		log.Printf("Error encoding json %s", err)
+	}
 }
